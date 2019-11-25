79c6e8908bbf4f10b683af5874b9f12a60a0508c
Brendan Horst <yoshistar2000@gmail.com>
2019-11-25 15:30:26 -0500

    added the ability to click on a square and an X appears

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ const players = ['X', 'O'];
 for (let string of grid) {
   let box = document.createElement('div');
   box.classList.add('cell');
+  box.addEventListener('mousedown', event => {
+    if(event.target.textContent === '') event.target.textContent = players[turn];
+  });
   document.getElementById('gridContainer').appendChild(box);
 }
