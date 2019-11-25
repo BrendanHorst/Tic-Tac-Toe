56cfa98c837c99c313a7b3fcf2f2ecd4b6b88985
Andrew Scholly <SATmaster728@gmail.com>
2019-11-25 16:23:35 -0500

    added checking if rows or columns have matching x's, rows have unfixed issue

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-let grid = ['', '', '', '', '', '', '', '', '']
+let grid = []
 let turn = 0;
 const players = ['X', 'O'];
 
-for (let string of grid) {
-  let box = document.createElement('div');
-  box.classList.add('cell');
-  box.addEventListener('mousedown', event => {
-    if(event.target.textContent === '') event.target.textContent = players[turn];
-    if (turn == 0){
-      turn = 1;
+function win() {
+  for (let count = 0; count <= 2; count++) {
+    if (grid[count].textContent == 'X' && grid[count + 3].textContent == 'X' &&
+    grid[count + 6].textContent == 'X') {
+      console.log("you win");
     }
-    else{
-      turn = 0;
+  }
+  for (let count2 = 0; count <= 6; count += 3) {
+    if (grid[count].textContent == 'X' && grid[count + 1].textContent == 'X' &&
+    grid[count + 2].textContent == 'X') {
+      console.log("you win");
     }
-
-  });
-  document.getElementById('gridContainer').appendChild(box);
-}
+  }
+  for (let count2 = 0; count2 <= 8; count2++) {
+    let box = document.createElement('div');
+    grid.push(box);
+    box.classList.add('cell');
+    box.addEventListener('mousedown', event => {
+      if (event.target.textContent === '') event.target.textContent = players[turn];
+      if (turn == 0) {
+        turn = 1;
+      } else {
+        turn = 0;
+      }
+      win();
+    });
+    document.getElementById('gridContainer').appendChild(box);
+  }
