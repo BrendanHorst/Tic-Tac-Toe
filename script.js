b85a48574ab958dbcc7f73202fb5af737216ca62
Andrew Scholly <SATmaster728@gmail.com>
2019-11-26 12:19:14 -0500

    put in all win conditions

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ let turn = 0;
 const players = ['X', 'O'];
 
 function win() {
+  if (grid[0].textContent == players[turn] && grid[4].textContent == players[turn] &&
+  grid[8].textContent == players[turn]) {
+    console.log("you win");
+  }
+  if (grid[2].textContent == players[turn] && grid[4].textContent == players[turn] &&
+  grid[6].textContent == players[turn]) {
+    console.log("you win");
+  }
   for (let count = 0; count <= 2; count++) {
-    if (grid[count].textContent == 'X' && grid[count + 3].textContent == 'X' &&
-    grid[count + 6].textContent == 'X') {
+    if (grid[count].textContent == players[turn] && grid[count + 3].textContent == players[turn] &&
+    grid[count + 6].textContent == players[turn]) {
       console.log("you win");
     }
   }
   for (let count = 0; count <= 6; count += 3) {
-    if (grid[count].textContent == 'X' && grid[count + 1].textContent == 'X' &&
-    grid[count + 2].textContent == 'X') {
+    if (grid[count].textContent == players[turn] && grid[count + 1].textContent == players[turn] &&
+    grid[count + 2].textContent == players[turn]) {
       console.log("you win");
     }
   }
@@ -22,12 +30,12 @@ function win() {
     box.classList.add('cell');
     box.addEventListener('mousedown', event => {
       if (event.target.textContent === '') event.target.textContent = players[turn];
+      win();
       if (turn == 0) {
         turn = 1;
       } else {
         turn = 0;
       }
-      win();
     });
     document.getElementById('gridContainer').appendChild(box);
   }
